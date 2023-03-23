9d291436295337638c580ebd70e923e10a6f58fd
xor123 <xor@mail.ru>
2023-03-23 19:24:03 +0300

    Убран запуск приложения через builder

--- src/main/java/club/kanban/j2aa/J2aaApp.java
+++ src/main/java/club/kanban/j2aa/J2aaApp.java
@@ -18,8 +18,8 @@ import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 import javax.annotation.PostConstruct;
 import javax.net.ssl.SSLPeerUnverifiedException;
@@ -239,7 +239,7 @@ public class J2aaApp extends JFrame implements UILogInterface {
             }
         }
 
-        new SpringApplicationBuilder(J2aaConfig.class).headless(false).run(args);
+        SpringApplication.run(J2aaApp.class);
     }
 
     @PostConstruct
